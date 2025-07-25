18209cb5254516c1e1741e8bbabb5eaa0f25124a
Waseque Siddiqui <wasequesiddiqui@hotmail.com>
2025-07-25 23:45:04 +0530

    25 JUL 2025

--- Data_visualization.py
+++ Data_visualization.py
@@ -1,36 +1,20 @@
-import streamlit as st
-import pandas as pd
-import altair as alt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-st.markdown("<h1 style='text-align: center;'>Data Visualizer App on StreamLit</h1>",unsafe_allow_html=True)
-st.markdown("---",unsafe_allow_html=True)
-files_names = list()
-files = st.file_uploader("upload multiple files", type=["csv"], accept_multiple_files=True)
+positions = [(2.5, 2.5), (4, 4), (1, 4), (1, 1), (4, 1), (2.5, 2.5)]
 
-if files:
-    for file in files:
-        files_names.append(file.name)
-    print(files_names)
-    selected_files = st.multiselect("Select Files: ", options=files_names)
-    if selected_files:
-        option = st.radio("Select Entity Against Date ", options=['Price','Open','High','Low','Vol.','Change %'])
-        if option!=None:
-            for file in files:
-                if file.name in selected_files:
-                    chart_data = pd.read_csv(file)
-                    chart_data['Change %'] = chart_data['Change %'].str.replace('%', '')
-                    chart_data["Change %"] = chart_data["Change %"].astype(float).fillna(0.0)
-                    chart_data["Color_Val"] = "Green"
-                    chart_data.loc[(chart_data["Change %"]<0),"Color_Val"] = "Red"
-                    c = (
-                        alt.Chart(chart_data,title = "Displaying plot for " + option + " for " + file.name)
-                        .mark_circle()
-                        .encode(x="Date"
-                                , y=option
-                                , size="Change %"
-                                , tooltip=["Open", "High", "Low"]
-                                , color= alt.Color('Color_Val',legend=None)
-                                )
-                        ).interactive()
-                    st.altair_chart(c)
+fig, ax = plt.subplots()
+# Initialize at the first position
+player_dot, = ax.plot([positions[0][0]], [positions[0][1]], 'ro', markersize=10)
+court_lines = [(0, 0), (0, 5), (5, 5), (5, 0), (0, 0)]
 
+ax.plot(*zip(*court_lines), color='gray')
+ax.set_xlim(-1, 6)
+ax.set_ylim(-1, 6)
+
+def update(frame):
+    player_dot.set_data([positions[frame][0]], [positions[frame][1]])
+    return player_dot,
+
+ani = animation.FuncAnimation(fig, update, frames=len(positions), interval=500, blit=True)
+ani.save('footwork_demo.gif', writer='pillow')
