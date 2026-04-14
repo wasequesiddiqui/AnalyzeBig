264ee92d52cf12f05b37a373c225e49c3fb42694
Waseque Siddiqui <wasequesiddiqui@hotmail.com>
2026-04-14 13:05:23 +0530

    Update Equity_Evaluator.py

--- Equity_Finder/Equity_Evaluator.py
+++ Equity_Finder/Equity_Evaluator.py
@@ -14,6 +14,46 @@ import EQUITY_ANALYSER as ea
 
 from plotly.subplots import make_subplots
 
+st.set_page_config(layout="wide",
+                   page_title="Analysing Nifty 50 & Next 50 Stocks",
+                   page_icon="📈"
+                   )
+# Main code for analysis and visualization of nifty 50 data
+# using streamlit and plotly
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"] {
+        background-color: #f0f8e5; /* Light olive green color */
+    }
+    /* Change the main content background color */
+    [data-testid="stAppViewContainer"] {
+        background-color: #fffdf0; /* Light gray color */
+    }
+    /* Set a complementary color for the titles and headers */
+    h1, h2, h3 {
+        color: #787355; /* A warm, dark gray for readability */
+    }
+    /* Customize the sidebar header */
+    .css-1jc7h9d, .e1ewe9a51, .e1ewe9a52 { /* These are Streamlit's generated CSS classes for the header */
+        background-color: #008080; /* Your desired color, e.g., Teal */
+        color: white; /* Change the text color for better contrast */
+    }
+    [data-testid="stToolbar"] {
+        background-color: #cab161; /* golden background */
+        padding: 10px;
+        border-radius: 5px;
+    }
+    [data-testid="stToolbar"] a {
+        color: #927748; /* deep gold text */
+        text-decoration: none;
+        font-weight: bold;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # --- 1. INITIALIZE SESSION STATE ---
 if 'combined_results' not in st.session_state:
     st.session_state.combined_results = None
